f94285da7c7ef91546f68b5d994205066b6458ec
Truong Minh Huu <minhhuu0705@gmail.com>
2024-05-22 02:40:29 +0700

    code

---
+++ src/main/java/com/fit/se/DemoGithubActionsApplication.java
@@ -0,0 +1,28 @@
+package com.fit.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class DemoGithubActionsApplication {
+
+	@GetMapping("/name")
+	public String welcome() {
+		return "Truong Minh Huu";
+	}
+
+//	echo "# github-actions-demo" >> README.md
+//	git init
+//	git add README.md
+//	git commit -m "first commit"
+//	git branch -M main
+//	git remote add origin https://github.com/trngmhuu/github-actions-demo.git
+//	git push -u origin main
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubActionsApplication.class, args);
+	}
+
+}
